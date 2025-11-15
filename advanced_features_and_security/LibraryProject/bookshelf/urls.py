b12c5d5e2d869031ca8a64b8fe0a86def66e5925
Aicha <aichakelia01@gmail.com>
2025-11-15 17:45:00 +0100

    Completed Book permissions task with groups, views, urls, and documentation

---
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('book/<int:book_id>/', views.view_book, name='view_book'),
+    path('book/<int:book_id>/edit/', views.edit_book, name='edit_book'),
+]
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import get_object_or_404, render
+from .models import Book
 
-# Create your views here.
+# Only users with can_view permission can access this view
+@permission_required('bookshelf.can_view', raise_exception=True)
+def view_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'bookshelf/book_detail.html', {'book': book})
+
+# Only users with can_edit permission can access this view
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    # your edit logic here
+    return render(request, 'bookshelf/book_edit.html', {'book': book})
