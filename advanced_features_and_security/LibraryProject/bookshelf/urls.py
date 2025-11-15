db2559d30efbf57f349c007189bb9280a74f1553
Aicha <aichakelia01@gmail.com>
2025-11-15 17:52:51 +0100

    tweaking

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('book/<int:book_id>/', views.view_book, name='view_book'),
     path('book/<int:book_id>/edit/', views.edit_book, name='edit_book'),
+    path('books/', views.book_list, name='book_list'),
 ]
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,6 +4,12 @@ from .models import Book
 
 # Only users with can_view permission can access this view
 @permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+# Only users with can_view permission can access this view
+@permission_required('bookshelf.can_view', raise_exception=True)
 def view_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     return render(request, 'bookshelf/book_detail.html', {'book': book})
