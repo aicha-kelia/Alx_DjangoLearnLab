51a176e9558bbb1f4295cba49697fbcb8b61ccf8
Aicha <aichakelia01@gmail.com>
2025-11-17 20:22:47 +0100

    remove old folder

--- advanced_features_and_security_OLD/LibraryProject/bookshelf/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('book/<int:book_id>/', views.view_book, name='view_book'),
-    path('book/<int:book_id>/edit/', views.edit_book, name='edit_book'),
-    path('books/', views.book_list, name='book_list'),
-]
--- advanced_features_and_security_OLD/update.md
+++
@@ -1,3 +0,0 @@
-﻿book = Book.objects.get(id=1)
-book.title = "Nineteen Eighty-Four"
-book.save()
