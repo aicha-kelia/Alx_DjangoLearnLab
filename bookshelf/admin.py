5bb124748e3f4c92bc2216436879144c9e16ab24
Aicha <aichakelia01@gmail.com>
2025-11-17 22:11:56 +0100

    Fix: added LibraryProject structure

---
+++ bookshelf/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Book, CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('author', 'publication_year')
+    search_fields = ('title', 'author')
+
+admin.site.register(Book, BookAdmin)
---
+++ bookshelf/delete.md
@@ -0,0 +1,5 @@
+﻿from bookshelf.models import Book
+book = Book.objects.get(id=1)
+book.delete()
+Book.objects.all()
+# Output: <QuerySet []>
