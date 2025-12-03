fbf0cd5bf0b7aa167f8ff5af36bdb9e2d3add671
Aicha <aichakelia01@gmail.com>
2025-12-03 20:17:22 +0100

    templates

--- LibraryProject/bookshelf/views.py
+++ LibraryProject/bookshelf/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Book
+from .forms import BookForm
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+def form_example(request):
+    form = BookForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
