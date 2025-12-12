8cb224e02e3cca1d738303b19d6f3db37479c27f
Aicha <aichakelia01@gmail.com>
2025-12-12 20:11:26 +0100

    Update serializers with CharField()

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,11 +5,12 @@ from rest_framework.authtoken.models import Token
 User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()
     
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
+        extra_kwargs = {'password': {'write_only': True}}
     
     def create(self, validated_data):
         user = User.objects.create_user(
@@ -25,11 +26,11 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
 class UserLoginSerializer(serializers.Serializer):
     username = serializers.CharField()
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
-        read_only_fields = ['id']
+        read_only_fields = ['id', 'username']
